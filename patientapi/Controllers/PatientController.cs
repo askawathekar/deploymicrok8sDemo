235a44da7d1ba35a016c3de81defab0851352995
askawathekar <anandkawathekar20@gmail.com>
2021-07-07 16:43:15 +0530

    okay

---
+++ patientapi/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PatientAPI.Models;
+
+namespace patientapi.Controllers
+{
+
+      [Route("PatientController")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        PatientDb database;
+
+        public PatientController()
+        {
+            database = new PatientDb();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPatient()
+        {
+            return Ok(database);
+        }
+    }
+}
---
+++ patientapi/Models/Patient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientAPI.Models
+{
+  
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string PatientName { get; set; }
+    }
+
+
+
+    public class PatientDb : List<Patient>
+    {
+        public PatientDb()
+        {
+            Add(new Patient() { PatientID = 11, PatientName = "Mayur" });
+            Add(new Patient() { PatientID = 12, PatientName = "Aditya" });
+        }
+    }
+}
